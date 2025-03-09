c988110702bc55350a0f2a22a741b3ab1f2fd492
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2025-03-08 19:50:19 -0600

    Correcting lazyload-css.js (and minified version) for subpages
    The function relies on the unique commit code for both subpages in order to perform the strict equality string checking for XSS attack preventions.

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -3,9 +3,10 @@ const decodeHTML = function(html){
   const textarea = document.createElement('textarea');
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
-  const target_str_subsites = '<link href="/assets/css/style.css" rel="stylesheet" fetchpriority="low">';
-  const escapedHTML = textarea.value.trim();
-  if (escapedHTML === target_str || escapedHTML === target_str_subsites) {
+  const target_str_subsite1 = '<link href="/assets/css/style.css?v=2b2adc92bab78443eef6b43efb7294fdb8dc9e1f" rel="stylesheet" fetchpriority="low">';
+  const target_str_subsite2 = '<link href="/assets/css/style.css?v=f8db67769659eb759bd7c29d124aca65f1248f60" rel="stylesheet" fetchpriority="low">';
+  const escapedHTML = textarea.textContent.trim();
+  if (escapedHTML === target_str || escapedHTML === target_str_subsite1 || escapedHTML === target_str_subsite2) {
     return escapedHTML;
   }
   else {
