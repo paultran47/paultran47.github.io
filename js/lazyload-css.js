045b3af50d5cd1edbb7c84185d5c05b84a64781a
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2025-03-09 03:15:37 -0500

    Updated lazyload-css.js with subsite2 SHA

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -4,7 +4,7 @@ const decodeHTML = function(html){
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
   const target_str_subsite1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=7d395109f15cd5cef9e978050ebab777553e592f" rel="stylesheet" fetchpriority="low">';
-  const target_str_subsite2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=a75545809cf24ebebb02839bc766fa35d60d9fa0" rel="stylesheet" fetchpriority="low">';
+  const target_str_subsite2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=f469dae877b2d1e0d1d0047a1e8a8de1c9987179" rel="stylesheet" fetchpriority="low">';
   const escapedHTML = textarea.value.trim();
   const sanitisedHTML = escapedHTML.replace(/(javascript:|data:|vbscript:)/gi, '');
   if (sanitisedHTML === target_str || sanitisedHTML === target_str_subsite1 || sanitisedHTML === target_str_subsite2) {
