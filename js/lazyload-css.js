97863f91f75209b1964fc7437fe1a66615b50930
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2025-03-09 00:23:18 -0600

    Improved security of lazyload-css.js and lazyload-css-min.js
    For lazyload-css.js, I've made two explicit mini-functions for escaping potentially unsafe HTML and unescaping safe HTML. In the former mini-function, the unescaped string is checked with strict equality against any of the 3 vetted strings (also escaped). If it is passed, the output will then be unescaped to then move onto the next step of the function.
    
    For lazyload-css-min.js, I've used the advanced optimisation for more savings.

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -1,21 +1,25 @@
 /* Functionality that lazy-loads non-critical CSS */
-const decodeHTML = function(html){
-  const textarea = document.createElement('textarea');
+const textarea = document.createElement('textarea');
+function escapeHTML(html) {
   textarea.textContent = html;
-  const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
-  const target_str_subsite1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=43689b4a5deb69920fb8ecdc03d8039b7129af38" rel="stylesheet" fetchpriority="low">';
-  const target_str_subsite2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=d68d18529c2205a0303115c6b8529323f961fd40" rel="stylesheet" fetchpriority="low">';
-  const escapedHTML = textarea.textContent.trim();
-  if (escapedHTML === target_str || escapedHTML === target_str_subsite1 || escapedHTML === target_str_subsite2) {
-    return escapedHTML;
+  const target_str = '&lt;link href="/css/style-min.css" rel="stylesheet" fetchpriority="low"&gt;';
+  const target_str_subsite1 = '&lt;link href="/econ-grad-app-deadlines/assets/css/style.css?v=43689b4a5deb69920fb8ecdc03d8039b7129af38" rel="stylesheet" fetchpriority="low"&gt;';
+  const target_str_subsite2 = '&lt;link href="/latex-cv-with-biblatex/assets/css/style.css?v=a75545809cf24ebebb02839bc766fa35d60d9fa0" rel="stylesheet" fetchpriority="low"&gt;';
+  const safe_str = textarea.innerHTML.trim();
+  if (safe_str === target_str || safe_str === target_str_subsite1 || safe_str === target_str_subsite2) {
+    return safe_str;
   }
   else {
     return false;
   }
-};
+}
+function unescapeHTML(html) {
+  textarea.innerHTML = html;
+  return textarea.textContent;
+}
 const getItemsFromContainerText = function(container, selector){
   const parser = new DOMParser();
-  const parsedHtml = parser.parseFromString(decodeHTML(container.textContent), 'text/html');
+  const parsedHtml = parser.parseFromString(unescapeHTML(escapeHTML(container.textContent)), 'text/html');
   return parsedHtml.querySelectorAll(selector);
 };
 function loadCss(){
