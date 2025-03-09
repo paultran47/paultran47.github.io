36d3044c14abaa2d29a8863c169bf04428b4b014
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2025-03-09 04:24:26 -0500

    Updated subpage SHAs

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -3,8 +3,8 @@ const decodeHTML = function(html){
   const textarea = document.createElement('textarea');
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=653fd0ae7714d6c8ca6904f1b537ac745ce2bfd5" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=bc2ee82f03bd9bf24e7cd3f228f621e90ff7988a" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=c58f29666cd68d9ab268d5efaf5f66ab8a97dd05" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=9e8c04b7c8640877bafd7680e5e0e55a8fe90257" rel="stylesheet" fetchpriority="low">';
   const escapedHTML = textarea.value.trim();
   const sanitisedHTML = escapedHTML.replace(/(javascript:|data:|vbscript:)/gi, '');
   if (sanitisedHTML === target_str || sanitisedHTML === target_str_subpage1 || sanitisedHTML === target_str_subpage2) {
