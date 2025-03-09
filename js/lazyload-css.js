c10ed348822d01ddc7ae444d6ed3e512b14fc834
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2025-03-09 00:40:05 -0600

    Reverting back to existed secured lazyload-css.js and lazyload-css-min.js
    .textContent already effectively escapes HTML by treating any given string as literal string, not markup.
    
    The existing strict equality conditional statements and "return false;" prevents any nefarious strings from passing on.

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -1,25 +1,21 @@
 /* Functionality that lazy-loads non-critical CSS */
-const textarea = document.createElement('textarea');
-function escapeHTML(html) {
+const decodeHTML = function(html){
+  const textarea = document.createElement('textarea');
   textarea.textContent = html;
-  const target_str = '&lt;link href="/css/style-min.css" rel="stylesheet" fetchpriority="low"&gt;';
-  const target_str_subsite1 = '&lt;link href="/econ-grad-app-deadlines/assets/css/style.css?v=43689b4a5deb69920fb8ecdc03d8039b7129af38" rel="stylesheet" fetchpriority="low"&gt;';
-  const target_str_subsite2 = '&lt;link href="/latex-cv-with-biblatex/assets/css/style.css?v=a75545809cf24ebebb02839bc766fa35d60d9fa0" rel="stylesheet" fetchpriority="low"&gt;';
-  const safe_str = textarea.innerHTML.trim();
-  if (safe_str === target_str || safe_str === target_str_subsite1 || safe_str === target_str_subsite2) {
-    return safe_str;
+  const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
+  const target_str_subsite1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=43689b4a5deb69920fb8ecdc03d8039b7129af38" rel="stylesheet" fetchpriority="low">';
+  const target_str_subsite2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=a75545809cf24ebebb02839bc766fa35d60d9fa0" rel="stylesheet" fetchpriority="low">';
+  const escapedHTML = textarea.value.trim();
+  if (escapedHTML === target_str || escapedHTML === target_str_subsite1 || escapedHTML === target_str_subsite2) {
+    return escapedHTML;
   }
   else {
     return false;
   }
-}
-function unescapeHTML(html) {
-  textarea.innerHTML = html;
-  return textarea.textContent;
-}
+};
 const getItemsFromContainerText = function(container, selector){
   const parser = new DOMParser();
-  const parsedHtml = parser.parseFromString(unescapeHTML(escapeHTML(container.textContent)), 'text/html');
+  const parsedHtml = parser.parseFromString(decodeHTML(container.textContent), 'text/html');
   return parsedHtml.querySelectorAll(selector);
 };
 function loadCss(){
