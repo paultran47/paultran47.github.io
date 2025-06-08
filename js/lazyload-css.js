e42839afcf96c7167105d4acceed9c50aab0c8b7
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2025-06-07 21:01:35 -0500

    Updated JMP information

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -4,7 +4,7 @@ const decodeHTML = function(html){
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
   const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=ed24bb4a2300f94ae0a41f7a093a849c45a49286" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=3fbab191890a4e7319aa4f606ff976970cfcaf20" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=94b785e1fcc133fda2c6bb1b74674e89215a858c" rel="stylesheet" fetchpriority="low">';
   const escapedHTML = textarea.value.trim();
   const sanitisedHTML = escapedHTML.replace(/(javascript:|data:|vbscript:)/gi, '');
   if (sanitisedHTML === target_str || sanitisedHTML === target_str_subpage1 || sanitisedHTML === target_str_subpage2) {
