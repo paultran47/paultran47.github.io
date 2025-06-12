7c029546be6d03ffbab127fcf406f2ff4f057a20
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2025-06-12 13:48:03 -0500

    Updated research section
    Updated research summary/interests spiel in summary section of page. Also corrected CSS for research page to be responsive to all device dimensions.

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -3,8 +3,8 @@ const decodeHTML = function(html){
   const textarea = document.createElement('textarea');
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=ed24bb4a2300f94ae0a41f7a093a849c45a49286" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=94b785e1fcc133fda2c6bb1b74674e89215a858c" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=44cc04c8ebc2ffdbd5b894bb4c79d6634e487e99" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=0659f16b25cdcb6df26d50f3a4df1bb3949b8e20" rel="stylesheet" fetchpriority="low">';
   const escapedHTML = textarea.value.trim();
   const sanitisedHTML = escapedHTML.replace(/(javascript:|data:|vbscript:)/gi, '');
   if (sanitisedHTML === target_str || sanitisedHTML === target_str_subpage1 || sanitisedHTML === target_str_subpage2) {
