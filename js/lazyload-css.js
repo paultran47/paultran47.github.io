f7b4a216cb9f7c8b823b7edcc393280ac32104d4
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2025-07-30 20:58:44 -0500

    Updated website for academic market
    Made JMP publicly visible, made teaching statement publicly available, moved JMP to working paper

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -4,7 +4,7 @@ const decodeHTML = function(html){
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
   const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=44cc04c8ebc2ffdbd5b894bb4c79d6634e487e99" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=0659f16b25cdcb6df26d50f3a4df1bb3949b8e20" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=e7d9fdba0ac92d3375920e21ea861df263dba2d1" rel="stylesheet" fetchpriority="low">';
   const escapedHTML = textarea.value.trim();
   const sanitisedHTML = escapedHTML.replace(/(javascript:|data:|vbscript:)/gi, '');
   if (sanitisedHTML === target_str || sanitisedHTML === target_str_subpage1 || sanitisedHTML === target_str_subpage2) {
