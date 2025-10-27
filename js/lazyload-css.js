a4571b45262f3508b0a1021410807acf810c783f
paultran47 <gwong.lee@gmail.com>
2025-10-27 03:31:43 -0500

    Updated SHA for sub-pages

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -3,8 +3,8 @@ const decodeHTML = function(html){
   const textarea = document.createElement('textarea');
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=fe5431114d36ce8d753438e68af001e44e06a426" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=28a6b776cca873b06be9e5f70f127f287d722db8" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=ce7e17644dcd5ce43cc1450f0b25e1f76017cf2e" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=cd4d450682ba2d6359b1d6c2cad05b084ca0ad7c" rel="stylesheet" fetchpriority="low">';
   const escapedHTML = textarea.value.trim();
   const sanitisedHTML = escapedHTML.replace(/(javascript:|data:|vbscript:)/gi, '');
   if (sanitisedHTML === target_str || sanitisedHTML === target_str_subpage1 || sanitisedHTML === target_str_subpage2) {
