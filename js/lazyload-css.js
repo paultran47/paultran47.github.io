8e899cd21f5ba19fd3b127974607327f114fc0f9
paultran47 <gwong.lee@gmail.com>
2025-11-20 21:37:25 -0600

    Updated all relevant pages with email obfuscation
    Made mailto email links obfuscated with escaped HTML entities. Made text displays of email a combination of escaped HTML entities and a comment in-between the email address.
    
    The latter change is important because it allows people to copy my email address easily now whilst providing effective protection against vast majority of email skimmers.

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -3,7 +3,7 @@ const decodeHTML = function(html){
   const textarea = document.createElement('textarea');
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=ed718f461b658d322feede2a0c031b77e7a27cc5" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=dacc425c0e88ad30857aedd467895213cf50041d" rel="stylesheet" fetchpriority="low">';
   const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=70b55430634a70172718a87b928eceb48d347dd7" rel="stylesheet" fetchpriority="low">';
   const escapedHTML = textarea.value.trim();
   const sanitisedHTML = escapedHTML.replace(/(javascript:|data:|vbscript:)/gi, '');
