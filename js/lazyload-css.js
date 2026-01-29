cd23ef10c92d4518e6d5769ac7a68119eef845a2
paultran47 <gwong.lee@gmail.com>
2026-01-29 17:39:07 -0600

    Updated JMP presentations list

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -4,7 +4,7 @@ const decodeHTML = function(html){
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
   const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=273b007f710ff881c61059d7dcc7dc2bc0187d79" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=119e327831b3738d02e5816970e4aefcb8e8600b" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=34219fc2e47e218073665ce79ed6f2b6a2daa5b0" rel="stylesheet" fetchpriority="low">';
   const escapedHTML = textarea.value.trim();
   const sanitisedHTML = escapedHTML.replace(/(javascript:|data:|vbscript:)/gi, '');
   if (sanitisedHTML === target_str || sanitisedHTML === target_str_subpage1 || sanitisedHTML === target_str_subpage2) {
