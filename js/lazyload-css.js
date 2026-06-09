cc723a66e6b9640481c43a57844024a5eb6a3c7d
paultran47 <gwong.lee@gmail.com>
2026-06-09 01:55:23 -0500

    Updated SSH for subpage

--- js/lazyload-css.js
+++ js/lazyload-css.js
@@ -4,7 +4,7 @@ const decodeHTML = function(html){
   textarea.textContent = html;
   const target_str = '<link href="/css/style-min.css" rel="stylesheet" fetchpriority="low">';
   const target_str_subpage1 = '<link href="/econ-grad-app-deadlines/assets/css/style.css?v=e081978268fcf8d5dc6ee3e09b953d2b12e7ec2c" rel="stylesheet" fetchpriority="low">';
-  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=64bc3bef6d2078ff8821548a0835b652bbde31ec" rel="stylesheet" fetchpriority="low">';
+  const target_str_subpage2 = '<link href="/latex-cv-with-biblatex/assets/css/style.css?v=6f3b6045e245ad10f49e88173566c8fbf08c0e0b" rel="stylesheet" fetchpriority="low">';
   const escapedHTML = textarea.value.trim();
   const sanitisedHTML = escapedHTML.replace(/(javascript:|data:|vbscript:)/gi, '');
   if (sanitisedHTML === target_str || sanitisedHTML === target_str_subpage1 || sanitisedHTML === target_str_subpage2) {
